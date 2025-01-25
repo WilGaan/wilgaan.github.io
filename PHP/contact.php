49584962adbce9b5b0f870a1c50cac5126357aed
ThomasDeronne <thomas.deronne@etu.unice.fr>
2025-01-25 14:16:56 +0100

    ajout dropdown et changement navbar

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,39 +1,37 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collecting data
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+$persoMail = 'thderonne@gmail.com';
 
-    // email detail
-    $to = "thderonne@gmail.com";
-    $subject = "Un nouveau message du Portfolio";
-    $headers = "From: " . $email . "\r\n";
-    $headers .= "Reply-To: " . $email . "\r\n";
-    $headers .= "Content-type: text/html\r\n";
-
-    // email content
-    $email_content = "
-    <html>
-    <head>
-        <title>Contact Message</title>
-    </head>
-    <body>
-        <h2>New Contact Message</h2>
-        <p><strong>Name:</strong> {$name}</p>
-        <p><strong>Email:</strong> {$email}</p>
-        <p><strong>Message:</strong> {$message}</p>
-    </body>
-    </html>";
-
-    // Sending email
-    if (mail($to, $subject, $email_content, $headers)) {
-        echo "Message sent successfully.";
+if (isset($_POST['envoyer'])) {
+    if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i", $_POST['mail'])){
+        echo "<p>L'adresse mail entrée est incorrecte.</p>";
     } else {
-        echo "Failed to send message. Please try again later.";
+        $userMail = $_POST['mail'];
+        $userName = $_POST['nom'];
+
+        $entetes_du_mail = [];
+        $entetes_du_mail[] = 'MIME-Version: 1.0';
+        $entetes_du_mail[] = 'Content-type: text/html; charset=UTF-8';
+        $entetes_du_mail[] = 'From: Nom de votre site <' . $persoMail . '>';
+        $entetes_du_mail[] = 'Reply-To: Nom de votre site <' . $persoMail . '>';
+
+        $entetes_du_mail = implode("\r\n", $entetes_du_mail);
+
+        $sujet = '=?UTF-8?B?Formulaire de contact de ' . $nom . '?=';
+
+        $message = htmlentities($_POST['message'], ENT_QUOTES, 'UTF-8');
+
+        $message = nl2br($message);
+        if(mail($persoMail, $sujet, $message, $entetes_du_mail)){
+      
+            echo "<p>Le mail à été envoyé avec succès !</p>";
+          
+          }else{
+            
+            echo "<p>Une erreur est survenue, le mail n'a pas été envoyé.</p>";
+          
+          }
     }
-} else {
-    echo "Invalid request method.";
 }
+
 ?>
